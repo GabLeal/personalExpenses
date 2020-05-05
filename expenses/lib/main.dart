693177d374b813688f75e641205639cf7c6a513b
leal <gabriellealttrh@gmail.com>
2020-05-05 18:35:39 -0300

    add expenses

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
+import 'dart:math';
 
 
 main()=> runApp(ExpensesApp());
@@ -15,11 +18,15 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
+class MyHomePage extends StatefulWidget {
 
-  final _trasactions = [
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+    final _transactions = [
     Transaction(
       id: "t2", 
       title: "Tenis adidas", 
@@ -34,109 +41,62 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now()
     )
   ];
+
+  _addTransaction(String title, double value){
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title, 
+      value: value, 
+      date: DateTime.now()
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context){
+    showModalBottomSheet(
+      context: context, 
+      builder: (_){
+        return TransactionForm(_addTransaction);
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Despesas pessoais"),
         centerTitle: true,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Card(
-              color: Colors.blue,
-              child: Text("Grafico"),
-              elevation: 5,
-            ),
-          ),
-          Column(
-            children: _trasactions.map((tr){
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2
-                        )
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        "R\$ ${tr.value.toStringAsFixed(2)}",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        Text(
-                          DateFormat("d/MM/y").format(tr.date),
-                          style: TextStyle(
-                            color: Colors.grey
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: "Titulo"
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: "Valor (R\$)"
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      FlatButton(
-                        child: Text("Nova Transação"),
-                        textColor: Colors.purple,
-                        onPressed: (){
-                          print(titleController.text);
-                          print(valueController.text);
-                        },
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add), 
+            onPressed: ()=> _openTransactionFormModal(context)
           )
         ],
-      )
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text("Grafico"),
+                elevation: 5,
+              ),
+            ),
+            TransactionsList(_transactions),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: ()=> _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
-
 }
